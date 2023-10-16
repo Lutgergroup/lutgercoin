fc1c3b1b9b4ff947687ab31038e660932a60f654
kianabel <kian.leba@gmail.com>
2023-10-16 11:48:27 +0200

    drecks php

---
+++ save_email.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the email from the form
+    $email = $_POST["email"];
+
+    if (!empty($email)) {
+        // Define the file path
+        $file = "email.txt";
+
+        // Open the file for writing
+        $handle = fopen($file, "a");
+
+        // Write the email to the file
+        fwrite($handle, $email . PHP_EOL);
+
+        // Close the file
+        fclose($handle);
+
+        // Provide a response
+        echo "Email saved successfully.";
+    } else {
+        echo "Email is empty.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
